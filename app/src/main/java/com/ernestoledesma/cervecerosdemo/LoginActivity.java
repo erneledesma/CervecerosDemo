e1c1a07b53637ff17b190f82a2ba3931d4af1c76
Ernesto Ledesma <erneledesma@gmail.com>
2018-09-22 20:48:56 -0300

    Initial commit - Commit de la demo con las interfaces de login y Account

---
+++ app/src/main/java/com/ernestoledesma/cervecerosdemo/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.ernestoledesma.cervecerosdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ernestoledesma.cervecerosdemo.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+
+
+    public void goCreateAccount(View view){
+
+        Intent intent = new Intent(this,CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+
+}
+
+
+
