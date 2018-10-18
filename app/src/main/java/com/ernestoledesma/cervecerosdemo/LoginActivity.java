d898a99ec2a2546a40eaf928f818c1530b7a12aa
Ernesto Ledesma <erneledesma@gmail.com>
2018-10-18 08:05:19 -0300

    Se agrega la funcionabilidad de los fragment - cuarto  commits

--- app/src/main/java/com/ernestoledesma/cervecerosdemo/LoginActivity.java
+++ app/src/main/java/com/ernestoledesma/cervecerosdemo/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.ernestoledesma.cervecerosdemo.view.ContainerActivity;
 import com.ernestoledesma.cervecerosdemo.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -21,6 +22,13 @@ public class LoginActivity extends AppCompatActivity {
     public void goCreateAccount(View view){
 
         Intent intent = new Intent(this,CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void goHome(View view){
+
+        Intent intent = new Intent(this, ContainerActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/ernestoledesma/cervecerosdemo/view/ContainerActivity.java
+++ app/src/main/java/com/ernestoledesma/cervecerosdemo/view/ContainerActivity.java
@@ -1,7 +1,15 @@
 package com.ernestoledesma.cervecerosdemo.view;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ernestoledesma.cervecerosdemo.R;
+import com.ernestoledesma.cervecerosdemo.view.fragments.HomeFragment;
+import com.ernestoledesma.cervecerosdemo.view.fragments.ProfileFragment;
+import com.ernestoledesma.cervecerosdemo.view.fragments.SearchFragment;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class ContainerActivity extends AppCompatActivity {
 
@@ -9,5 +17,42 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
+
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottombar);
+        bottomBar.setDefaultTab(R.id.tab_home);
+
+        // Este listener es el que esta atento para mostrar el fragment de inicio
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId) {
+                switch (tabId){
+                    case R.id.tab_home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+
+                    case R.id.tab_user:
+                        ProfileFragment profileFragment = new ProfileFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+
+                    case R.id.tab_search:
+                        SearchFragment searchFragment = new SearchFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment)
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .addToBackStack(null).commit();
+                        break;
+                }
+            }
+        });
     }
+
+
+
+
 }
